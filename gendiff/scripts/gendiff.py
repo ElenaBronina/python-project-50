95bc450258034e0ff631510f9291862377420667
Elena Bronina <aplicaziya@yandex.ru>
2024-02-25 10:42:37 +0300

    add step 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,21 @@
-import argparse
+import json
 
-parser = argparse.ArgumentParser(prog = 'gendiff', description = 'Compares two configuration files and shows a difference.')
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-parser.add_argument('-f', '--format', help='set format of output')
-args = parser.parse_args()
-print(args.echo)
+def to_lower(str_):
+    return str(str_).lower()
+
+def diff(file1, file2):
+    data1 = json.load(open(file1))
+    data2 = json.load(open(file2))
+    diff = []
+    for key in sorted(data1.keys() | data2.keys()):
+        if key in data1 and key in data2 and data1[key] == data2[key]:
+            diff.append(f'  {key}: {to_lower(data1[key])}')
+        elif key in data1 and key in data2 and data1[key] != data2[key]:
+            diff.append(f'- {key}: {to_lower(data1[key])}')
+            diff.append(f'+ {key}: {to_lower(data2[key])}')
+        elif key in data1:
+            diff.append(f'- {key}: {to_lower(data1[key])}')
+        elif key in data2:
+            diff.append(f'+ {key}: {to_lower(data2[key])}')
+    diff_list = "\n ".join(diff)
+    return f'{{\n {diff_list}\n}}'
